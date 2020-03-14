c6a021ec574a1b0ca1e55ad3d8b3c5ced0b9179c
Ajit Kumar <dellevenjack@gmail.com>
2020-03-15 00:07:58 +0530

    fixed package.json

--- typings/cordova-typings.d.ts
+++ typings/cordova-typings.d.ts
@@ -7,3 +7,8 @@
 /// <reference path="..\.vscode\typings\cordova\plugins\Toast.d.ts"/>
 /// <reference path="../.vscode/typings/cordova/plugins/Toast.d.ts"/>
 /// <reference path="../.vscode/typings/cordova/plugins/FileTransfer.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Device.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/StatusBar.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Vibration.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/FileSystem.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/InAppBrowser.d.ts"/>
