38cb23f8368909776fa89b87a35f35d3a69f621b
Ajit <me@ajitkumar.dev>
2022-08-25 20:33:23 +0530

    version 1.6.0 beta ☞ New option find file ☞ Stability improvement ☞ UI improvement ☞ Fixed keyboard issues ☞ Fixed tap hold to select text ☞ Fixed loading files using FTP/SFTP ☞ Fixed File checking ☞ Fixed settings

--- plugins/cordova-plugin-server/index.d.ts
+++
@@ -1,7 +0,0 @@
-interface Server{
-  stop(onSuccess: () => void, onError: (error: any) => void): void;
-  send(id: string, data: any, onSuccess: () => void, onError: (error: any) => void): void;
-  port: number;
-}
-
-declare var CreateServer: (port: number, onSuccess: (msg: any) => void, onError: (err: any) => void) => Server;
