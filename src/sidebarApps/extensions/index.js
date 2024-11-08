deadlyjack/Acode

commit 45b8e3645269ed1dbb7d2c75fe4c9b26f0231908

fix(Plugin): Ads Been Display on uninstall in the Pro Version(in-app purchased)
This commit attempts to fix the Ads that are displayed For Plugin uninstallation when preformed via Plugin Menu/Context Menu/More(three dots)  on the Pro Version(In-App Purchased)

src/sidebarApps/extensions/index.js
@@ -368,7 +368,9 @@ async function uninstall(id) {
 			state.delete(state.storeUrl),
 		]);
 		acode.unmountPlugin(id);
-		if (!IS_FREE_VERSION && (await window.iad?.isLoaded())) {
+
+    // Show Ad If Its Free Version, interstitial Ad(iad) is loaded.
+		if (IS_FREE_VERSION && (await window.iad?.isLoaded())) {
 			window.iad.show();
 		}
 	} catch (err) {

commit 35676c9e8c892981c14c40066d6a72e30e2bfd45

Fix(SidebarApps): indentation of comment Caused by https://github.com/deadlyjack/Acode/commit/45b8e3645269ed1dbb7d2c75fe4c9b26f0231908

src/sidebarApps/extensions/index.js
@@ -369,7 +369,7 @@ async function uninstall(id) {
 		]);
 		acode.unmountPlugin(id);
 
-    // Show Ad If Its Free Version, interstitial Ad(iad) is loaded.
+    		// Show Ad If Its Free Version, interstitial Ad(iad) is loaded.
 		if (IS_FREE_VERSION && (await window.iad?.isLoaded())) {
 			window.iad.show();
 		}